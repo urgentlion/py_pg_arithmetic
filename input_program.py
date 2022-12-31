25c3609aca4cdfab5ea520c7b4cf62b489df3022
urgentlion <urgentlioninfo@gmail.com>
2022-12-31 14:39:15 -0800

    ...

---
+++ input_program.py
@@ -0,0 +1,11 @@
+test1 = int(input("Type a number: "))
+result = test1 + 5
+print(result)
+
+test2 = int(input("Type a number: "))
+result2 = test2 + 7
+print(result2)
+
+test3 = int(input("Type a number: "))
+result3 = test3 + 2
+print(result3)
